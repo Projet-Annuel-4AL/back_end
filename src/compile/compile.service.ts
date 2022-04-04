d7ac63e58c0b67ab45f9630e207b91e12302db73
Ilyess Naït Belkacem <iilyessnb@gmail.com>
2022-04-04 22:07:58 +0200

    add compile module

---
+++ src/compile/compile.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { Observable, map } from 'rxjs';
+import { AxiosResponse } from 'axios';
+
+@Injectable()
+export class CompileService {
+  constructor(private readonly http: HttpService) {}
+
+  async compile(
+    code: string,
+    language: string,
+    input: string,
+  ): Promise<Observable<AxiosResponse<any>>> {
+    if (language == 'Python') {
+      language = 'py';
+    } else if (language == 'Java') {
+      language = 'java';
+    }
+    // else if (language == 'C++') {
+    //   language = 'cpp';
+    // }
+
+    const body = JSON.stringify({
+      code: code,
+      language: language,
+      input: input,
+    });
+
+    return this.http
+      .post('https://codexweb.netlify.app/.netlify/functions/enforceCode', body)
+      .pipe(map((response) => response.data.output));
+  }
+}
