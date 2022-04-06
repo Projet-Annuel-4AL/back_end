eb437b1025997754133343eb59a436c0cd7bd319
Angoston Lucas <lucasangostonpro@gmail.com>
2022-04-06 11:02:26 +0200

    improve workflow

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,7 +1,7 @@
 describe('AppController', () => {
   describe('root', () => {
     it('should return user profile', () => {
-      const test = 'hello';
+      const test = 'hello1';
       expect('hello').toBe(test);
     });
   });
