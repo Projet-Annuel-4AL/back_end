d010c5b846a0436677c8f59e8491dd5233679f4e
Ilyess Naït Belkacem <iilyessnb@gmail.com>
2022-04-08 15:02:42 +0200

    add script and main-class endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,18 +2,21 @@ import { Controller, Request, Get, Post, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './auth/local-auth-guard';
 import { AuthService } from './auth/auth.service';
 import { JwtAuthGuard } from './auth/jwt-auth-guard';
-import { CompileService } from './compile/compile.service';
+import { CompilerService } from './compiler/compiler.service';
+import { Language } from './run-code/enum/language';
+import { ExecutionScriptGenerateManagerService } from './run-code/execution-script-generate-manager/execution-script-generate-manager.service';
+import { FileManagerService } from './run-code/file-manager/file-manager.service';
 
 @Controller()
 export class AppController {
   constructor(
     private authService: AuthService,
-    private compileService: CompileService,
+    private compileService: CompilerService,
   ) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
-  async login(@Request() req) {
+  login(@Request() req) {
     return this.authService.login(req.user);
   }
 
@@ -31,5 +34,20 @@ export class AppController {
       req.body.language,
       req.body.input,
     );
+  }
+
+  @Get('script')
+  async script() {
+    const language: Language = Language.Java;
+    ExecutionScriptGenerateManagerService.generateExecutionScript(language);
+  }
+
+  @Post('main-class')
+  async mainClass(@Request() req) {
+    const language: Language = Language.Java;
+    const path: string =
+      FileManagerService.getFolderName(language) +
+      FileManagerService.getFileName(language);
+    FileManagerService.createFile(path, req.body.code);
   }
 }
