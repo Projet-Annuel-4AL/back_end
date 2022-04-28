4aea90c9c8ac0235e4accc2576d2a03fd63c2cd6
Ilyess Naït Belkacem <iilyessnb@gmail.com>
2022-04-28 12:01:09 +0200

    add post mudole

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,16 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { CodeRunnerModule } from './code-runner/code-runner.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, CodeRunnerModule, CodeRunnerModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    CodeRunnerModule,
+    CodeRunnerModule,
+    PostsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
