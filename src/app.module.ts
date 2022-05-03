1fa807b87e8beb1f02eb112def1f25f12dbc7223
Ilyess Naït Belkacem <iilyessnb@gmail.com>
2022-05-03 14:11:38 +0200

    add BD connection properties

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,20 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { CodeRunnerModule } from './code-runner/code-runner.module';
 import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: ['dist/**/**.entity{.ts,.js}'],
+      synchronize: false,
+    }),
     AuthModule,
     UsersModule,
     CodeRunnerModule,
