9321e474a367b8ebf1f5426fb4bc0daabb8d96f4
Ilyess Naït Belkacem <iilyessnb@gmail.com>
2022-05-04 16:07:22 +0200

    import typeOrmModule

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
+import { UsersService } from '../users/users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Post } from './post.entity';
+import { User } from '../users/user.entity';
 
 @Module({
-  providers: [PostsService],
+  imports: [TypeOrmModule.forFeature([Post, User])],
+  providers: [PostsService, UsersService],
+  exports: [PostsService],
 })
 export class PostsModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Post } from '../posts/post.entity';
+import { User } from './user.entity';
+import { PostsService } from '../posts/posts.service';
 
 @Module({
-  providers: [UsersService],
+  imports: [TypeOrmModule.forFeature([Post, User])],
+  providers: [UsersService, PostsService],
   exports: [UsersService],
 })
 export class UsersModule {}
