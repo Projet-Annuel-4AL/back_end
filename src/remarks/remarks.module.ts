73631401347078a274be856ae91bd7cc99fa8b50
Ilyess Naït Belkacem <iilyessnb@gmail.com>
2022-06-12 11:45:04 +0200

    fix @Module import

--- src/remarks/remarks.module.ts
+++ src/remarks/remarks.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RemarksService } from './remarks.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Remark } from './remark.entity';
 
 @Module({
-  imports: [RemarksModule],
-
+  imports: [TypeOrmModule.forFeature([Remark])],
   providers: [RemarksService],
+  exports: [RemarksService],
 })
 export class RemarksModule {}
