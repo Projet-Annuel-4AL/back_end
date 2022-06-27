2f37c41bd8064511701894d1f94c231a61cbbb02
Ilyess Naït Belkacem <iilyessnb@gmail.com>
2022-06-27 21:11:56 +0200

    import TextsModule and CodesModules

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,14 +1,22 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
-import { UsersService } from '../users/users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from './post.entity';
 import { User } from '../users/user.entity';
 import { PostsController } from './posts.controller';
+import { Text } from './post-body/texts/text.entity';
+import { TextsModule } from './post-body/texts/texts.module';
+import { CodesModule } from './post-body/codes/codes.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post, User])],
-  providers: [PostsService, UsersService],
+  imports: [
+    TypeOrmModule.forFeature([Post, User, Text]),
+    TextsModule,
+    CodesModule,
+    UsersModule,
+  ],
+  providers: [PostsService],
   exports: [PostsService],
   controllers: [PostsController],
 })
