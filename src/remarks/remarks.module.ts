f6fc27218effd223ad1157a23b7636bc60244973
Ilyess Naït Belkacem <iilyessnb@gmail.com>
2022-06-27 21:13:30 +0200

    import PostsModule

--- src/remarks/remarks.module.ts
+++ src/remarks/remarks.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { RemarksService } from './remarks.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Remark } from './remark.entity';
+import { PostsModule } from '../posts/posts.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Remark])],
+  imports: [TypeOrmModule.forFeature([Remark]), PostsModule],
   providers: [RemarksService],
   exports: [RemarksService],
 })
