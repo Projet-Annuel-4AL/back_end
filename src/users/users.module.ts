34a49a1382e53949c1b2ad604a49385f802e7fc9
Ilyess Naït Belkacem <iilyessnb@gmail.com>
2022-06-27 21:13:10 +0200

    remove PostsModule from import

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Post } from '../posts/post.entity';
 import { User } from './user.entity';
-import { PostsService } from '../posts/posts.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post, User])],
-  providers: [UsersService, PostsService],
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [UsersService],
   exports: [UsersService],
 })
 export class UsersModule {}
